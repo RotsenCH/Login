bd5593f4ce873c2a94eed5656a5a08c11789440c
RotsenCH <117753370+RotsenCH@users.noreply.github.com>
2023-01-26 19:58:28 -0500

    Update Ventana_Login.java
    Agregar Main

--- src/Ventana_Login.java
+++ src/Ventana_Login.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class Ventana_Login {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class Ventana_Login {
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JPanel Panel_Principal;
+    private JButton ingresarButton;
+    private JButton salirButton;
+    private JLabel Jlabel_Usuario;
+    private JLabel Jlabel_Contraseña;
+    private JLabel Jlabel_Bienvenida;
+    private JLabel Ican_usuario;
+
+    public Ventana_Login(){
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+            }
+        });
+
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame =new JFrame("Ventana_Componentes");
+        frame.setContentPane(new Ventana_Login().Panel_Principal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
 }
+
